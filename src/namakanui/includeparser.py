6314e4e844d4aa6fc3168e93a65085a00e68a648
Ryan Berthold <r.berthold@eaobservatory.org>
2018-12-28 11:37:03 -1000

    add class for .ini [include] parsing

---
+++ src/namakanui/includeparser.py
@@ -0,0 +1,36 @@
+'''
+RMB 20181228
+ConfigParser derivative that supports an [include] section.
+Properly handles nested inclusions with absolute or relative paths.
+'''
+
+import sys
+import os
+import configparser
+
+class IncludeParser(configparser.ConfigParser):
+    def __init__(self, inifilename):
+        '''
+        Parse given .ini file, handling [include] sections.
+        NOTE: This is the only overridden method. Using read(), read_string(),
+              or other ConfigParser methods will not [include] properly.
+        '''
+        configparser.ConfigParser.__init__(self)
+        inifilename = os.path.realpath(inifilename.strip())
+        inidone = set()
+        include = {inifilename}
+        while inidone < include:
+            fname = next(iter(include - inidone))
+            inidir = os.path.dirname(fname) + '/'
+            inistr = open(fname).read()
+            self.read_string(inistr, source='<%s>'%(fname))
+            inidone.add(fname)
+            if 'include' in self:
+                for fname in self['include']:
+                    fname = fname.strip()
+                    if fname.startswith('/'):
+                        fname = os.path.realpath(fname)
+                    else:
+                        fname = os.path.realpath(inidir + fname)
+                    include.add(fname)
+
