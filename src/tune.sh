4d751b78521cd83e1ec3f74c313fdd04dfb5bf23
Ryan Berthold <r.berthold@eaobservatory.org>
2019-08-28 09:36:05 -1000

    use ini-1 dbm in tune.sh

--- src/tune.sh
+++ src/tune.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 # Tune a cartridge to the given LO GHz and optimize IF power
 # by using the dbm_table.py script.  Sets the "lock above reference" option
-# and starts the signal generator output 2 dBm below the value interpolated
+# and starts the signal generator output 1 dBm below the value interpolated
 # from the table in the agilent.ini file.
 # 
 # Usage:
@@ -10,4 +10,4 @@
 BAND=$1
 LO_GHZ=$2
 DBM_TABLE=/jac_sw/itsroot/install/namakanui/bin/Linux-x86_64/dbm_table.py
-${DBM_TABLE} "$BAND" "$LO_GHZ" "$LO_GHZ" 1 above ini-2
+${DBM_TABLE} "$BAND" "$LO_GHZ" "$LO_GHZ" 1 above ini-1
