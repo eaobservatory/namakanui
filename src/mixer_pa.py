6b5e50c4b0aa56b1f2f3baffe42efc8cda51a738
Ryan Berthold <r.berthold@eaobservatory.org>
2020-08-31 10:40:37 -1000

    add mixer_pa.py options and save PA readings

--- src/mixer_pa.py
+++ src/mixer_pa.py
@@ -37,7 +37,9 @@ parser = argparse.ArgumentParser()
 parser.add_argument('band', type=int, choices=[6,7])
 parser.add_argument('--lo')  # range
 parser.add_argument('--pa')  # range
+parser.add_argument('--load', nargs='?', default='hot')
 parser.add_argument('lock_polarity', nargs='?', choices=['below','above'], default='above')
+parser.add_argument('--note', nargs='?', default='', help='note for file header')
 args = parser.parse_args()
 
 band = args.band
@@ -47,15 +49,27 @@ pas = namakanui.util.parse_range(args.pa, maxlen=300)
 # create file header.
 # trying to plot this manually in topcat would be a nightmare anyway,
 # so just use the order that makes it easy to write the file.
-sys.stdout.write('#lo_ghz ')
+sys.stdout.write(time.strftime('# %Y%m%d %H:%M:%S HST\n', time.localtime()))
+sys.stdout.write('# %s\n'%(sys.argv))
+sys.stdout.write('# load: %s\n'%(args.load))
+sys.stdout.write('#\n')
+sys.stdout.write('#lo_ghz pa_3v pa_5v ')
 for pa in pas:
     for mixer in ['01', '02', '11', '12']:
-        sys.stdout.write('%s_%.3f '%(mixer, pa))
+        sys.stdout.write('ua%s_%03d '%(mixer, pa*100))
+    for po in ['0', '1']:
+        sys.stdout.write('vd%s_%03d '%(po, pa*100))
+    for po in ['0', '1']:
+        sys.stdout.write('id%s_%03d '%(po, pa*100))
+    for po in ['0', '1']:
+        sys.stdout.write('vg%s_%03d '%(po, pa*100))
 sys.stdout.write('\n')
 
-# init load controller and set to hot (ambient) load for this band
+# init load controller and set desired load (default hot)
 load = namakanui.load.Load(datapath+'load.ini', time.sleep, namakanui.nop, simulate=0)
-load.move('b%d_hot'%(band))
+if args.load == 'hot' or args.load == 'sky':
+    args.load = 'b%d_'%(band) + args.load
+load.move(args.load)
 
 # set agilent output to a safe level before setting ifswitch
 agilent = namakanui.agilent.Agilent(datapath+'agilent.ini', time.sleep, namakanui.nop, simulate=0)
@@ -87,6 +101,10 @@ for lo in los:
     for j,pa in enumerate(pas):
         cart._set_pa([pa,pa])
         time.sleep(0.05)
+        # check voltages
+        pa_3v = self.femc.get_cartridge_lo_pa_supply_voltage_3v(self.ca)
+        pa_5v = self.femc.get_cartridge_lo_pa_supply_voltage_5v(self.ca)
+        sys.stdout.write('%.3f %.3f '%(pa_3v, pa_5v))
         # average mixer currents
         n = 10
         uas = [0.0]*4
@@ -98,7 +116,19 @@ for lo in los:
             uas[i] /= n
             sys.stdout.write('%.3f '%(uas[i]))
             y[i][j].append(uas[i])
+        # amp feedback
+        pa_vd = [0.0]*2
+        pa_id = [0.0]*2
+        pa_vg = [0.0]*2
+        for po in range(2):
+            pa_vg[po] = self.femc.get_cartridge_lo_pa_gate_voltage(self.ca, po)
+            pa_vd[po] = self.femc.get_cartridge_lo_pa_drain_voltage(self.ca, po)
+            pa_id[po] = self.femc.get_cartridge_lo_pa_drain_current(self.ca, po)
+        sys.stdout.write('%.3f %.3f '%(pa_vd[0], pa_vd[1]))
+        sys.stdout.write('%.3f %.3f '%(pa_id[0], pa_id[1]))
+        sys.stdout.write('%.3f %.3f '%(pa_vg[0], pa_vg[1]))
     sys.stdout.write('\n')
+    sys.stdout.flush()
 
 # make a set of plots, one subplot per mixer
 logging.info('done.  creating plot...')
@@ -109,13 +139,5 @@ for i in range(4):
         p.plot(x,y[i][j])
     p.grid()
 show()
-
-                
-
-
-
-
-
-
 
 
