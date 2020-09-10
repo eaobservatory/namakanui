e275021227b1fd3a850dfb5edfa8bcbe50c7f77a
Ryan Berthold <r.berthold@eaobservatory.org>
2020-09-10 09:54:30 -1000

    add scripts dcm_synth, lo_sweep

---
+++ src/dcm_synth.py
@@ -0,0 +1,185 @@
+#!/local/python3/bin/python3
+'''
+dcm_synth.py   20200909 RMB
+
+TP2 test with the signal generator connected directly to the DCM quad input.
+
+TODO: Is there some way to guarantee that we're not accidentally
+      talking to the wrong signal generator?
+
+
+Copyright (C) 2020 East Asian Observatory
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
+'''
+
+import jac_sw
+import drama
+import sys
+import os
+import time
+import argparse
+import namakanui.agilent
+import namakanui.util
+
+import logging
+
+taskname = 'TRXS_%d'%(os.getpid())
+
+logging.root.setLevel(logging.INFO)
+logging.root.addHandler(logging.StreamHandler())
+
+binpath, datapath = namakanui.util.get_paths()
+
+# use explicit arguments to avoid confusion
+parser = argparse.ArgumentParser(description='''
+TP2 readings for direct synthesizer-to-DCM connection.
+''',
+  formatter_class=argparse.RawTextHelpFormatter)
+parser.add_argument('--level_only', action='store_true')
+parser.add_arguemnt('--bw_mhz', nargs='?', default='1000', help='BW MHz range, first:last:step')
+parser.add_argument('--if_ghz', nargs='?', default='6', help='IF GHz range, first:last:step')
+parser.add_argument('--dbm', nargs='?', default='14:-20:.1', help='dBm range, hi:lo:step')
+parser.add_argument('--dcm', help='starting DCM id (0-based); TP2 collected thru dcm+3.')
+args = parser.parse_args()
+
+
+bws = namakanui.util.parse_range(args.bw_mhz, maxlen=1e3)
+ifs = namakanui.util.parse_range(args.if_ghz, maxlen=1e3)
+dbms = namakanui.util.parse_range(args.dbm, maxlen=4e3)
+dcms = range(args.dcm, args.dcm+4)
+
+# set agilent output to a safe level
+agilent = namakanui.agilent.Agilent(datapath+'agilent.ini', time.sleep, namakanui.nop, simulate=0)
+agilent.set_dbm(agilent.safe_dbm)
+agilent.set_output(1)
+
+# write out a header for our output file
+sys.stdout.write(time.strftime('# %Y%m%d %H:%M:%S HST\n', time.localtime()))
+sys.stdout.write('# %s\n'%(sys.argv))
+sys.stdout.write('#\n')
+sys.stdout.write('#dbm if_ghz bw_mhz')
+for dcm in dcms:
+    sys.stdout.write(' dcm_%02d'%(dcm))
+sys.stdout.write('\n')
+sys.stdout.flush()
+
+def if_setup(adjust, bw_mhz, if_ghz):
+    # LEVEL_ADJUST 0=setup_only, 1=setup_and_level, 2=level_only
+    # BIT_MASK is DCMs to use: bit0=DCM0, bit1=DCM1, ... bit31=DCM31.
+    setup_type = ['setup_only', 'setup_and_level', 'level_only']
+    logging.info('setup IFTASK, LEVEL_ADJUST %d: %s', adjust, setup_type[adjust])
+    bitmask = 0
+    for dcm in dcms:
+        bitmask |= 1<<dcm
+    # TODO configurable IF_FREQ?  will 6 be default for both bands?
+    msg = drama.obey('IFTASK@if-micro', 'TEST_SETUP',
+                     NASM_SET='R_CABIN', BAND_WIDTH=bw_mhz, QUAD_MODE=4,
+                     IF_FREQ=if_ghz, LEVEL_ADJUST=adjust, BIT_MASK=bitmask).wait(90)
+    if msg.reason != drama.REA_COMPLETE or msg.status != 0:
+        if msg.status == 261456746:  # ACSISIF__ATTEN_ZERO
+            logging.warning('low attenuator setting from IFTASK.TEST_SETUP')
+        else:
+            logging.error('bad reply from IFTASK.TEST_SETUP: %s', msg)
+            return 1
+    return 0
+
+
+def set_bw(bw_mhz):
+    # this goes fast; probably doesn't do much.
+    logging.info('set bandwidth %g MHz', bw_mhz)
+    msg = drama.obey('IFTASK@if-micro', 'SET_DCM_BW', DCM=-1, MHZ=bw_mhz).wait(10)
+    if msg.reason != drama.REA_COMPLETE or msg.status != 0:
+        logging.error('bad reply from IFTASK.SET_DCM_BW: %s', msg)
+        return 1
+    return 0
+
+def set_lo2(lo2_mhz):
+    # this can take ~20s, or ~40s if it needs to change the coax switches.
+    # why so slow?
+    logging.info('set lo2 freq %g MHz', lo2_mhz)
+    msg = drama.obey('IFTASK@if-micro', 'SET_LO2_FREQ', LO2=-1, MHZ=lo2_mhz).wait(90)
+    if msg.reason != drama.REA_COMPLETE or msg.status != 0:
+        logging.error('bad reply from IFTASK.SET_LO2_FREQ: %s', msg)
+        return 1
+    return 0
+
+
+def main_loop():
+    for bw_mhz in bws:
+        if set_bw(bw_mhz):
+            return
+        
+        for if_ghz in ifs:
+            lo2_mhz = if_ghz*1e3 + 2500
+            if bw_mhz == 250:
+                lo2_mhz += 125
+            if set_lo2(lo2_mhz):
+                return
+            
+            agilent.set_hz_dbm(if_ghz*1e9, dbms[0])
+            if if_setup(2, bw_mhz, if_ghz):  # level only (won't actually set bw/lo2)
+                return
+            
+            for dbm in dbms:
+                agilent.set_dbm(dbm)
+                time.sleep(0.05)
+                # start IFTASK action
+                transid = drama.obey("IFTASK@if-micro", "WRITE_TP2", FILE="NONE", ITIME=0.1)
+                # get IFTASK reply
+                msg = transid.wait(5)
+                if msg.reason != drama.REA_COMPLETE or msg.status != 0:
+                    logging.error('bad reply from IFTASK.WRITE_TP2: %s', msg)
+                    return 1
+                sys.stdout.write('%.2f %g %g'%(dbm, if_ghz, bw_mhz))
+                for j,dcm in enumerate(dcms):
+                    sys.stdout.write(' %g'%(msg.arg['POWER%d'%(dcm)]))
+                sys.stdout.write('\n')
+                sys.stdout.flush()
+    # main_loop
+
+
+
+# the rest of this needs to be DRAMA to be able to talk to IFTASK.
+def MAIN(msg):
+    try:
+        adjust = [1,2][int(args.level_only)]
+        if if_setup(adjust, bws[0], ifs[0]):
+            return
+        main_loop()
+    finally:
+        # final timestamp
+        sys.stdout.write(time.strftime('# %Y%m%d %H:%M:%S HST\n', time.localtime()))
+        sys.stdout.flush()
+        # retune the receiver to get settings back to nominal
+        cart.tune(lo_ghz, 0.0)
+        drama.Exit('MAIN done')
+    # MAIN
+        
+
+try:
+    logging.info('drama.init...')
+    drama.init(taskname,
+               tidefile = datapath+'namakanui.tide',
+               buffers = [64000, 8000, 8000, 2000],
+               actions=[MAIN])
+    drama.blind_obey(taskname, "MAIN")
+    logging.info('drama.run...')
+    drama.run()
+finally:
+    logging.info('drama.stop...')
+    drama.stop()
+    logging.info('done.')
+    
---
+++ src/lo_sweep.py
@@ -0,0 +1,128 @@
+#!/local/python3/bin/python3
+'''
+lo_sweep.py     RMB 20200909
+
+Tune to a range of frequencies, save PA/SIS parameters in topcat ascii format.
+The goal is to create a plot similar to those in Ted Huang's email.
+
+
+Copyright (C) 2020 East Asian Observatory
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program.  If not, see <https://www.gnu.org/licenses/>.
+'''
+
+import jac_sw
+import sys
+import os
+import time
+import argparse
+import namakanui.cart
+import namakanui.agilent
+import namakanui.femc
+import namakanui.load
+import namakanui.ifswitch
+import namakanui.util
+import logging
+
+logging.root.setLevel(logging.INFO)
+logging.root.addHandler(logging.StreamHandler())
+
+binpath, datapath = namakanui.util.get_paths()
+    
+parser = argparse.ArgumentParser()
+parser.add_argument('band', type=int, choices=[6,7])
+parser.add_argument('--lo')  # range
+parser.add_argument('--load', nargs='?', default='hot')
+parser.add_argument('lock_polarity', nargs='?', choices=['below','above'], default='above')
+parser.add_argument('--note', nargs='?', default='', help='note for file header')
+args = parser.parse_args()
+
+band = args.band
+los = namakanui.util.parse_range(args.lo, maxlen=100e3)
+
+# create file header.
+# trying to plot this manually in topcat would be a nightmare anyway,
+# so just use the order that makes it easy to write the file.
+sys.stdout.write(time.strftime('# %Y%m%d %H:%M:%S HST\n', time.localtime()))
+sys.stdout.write('# %s\n'%(sys.argv))
+sys.stdout.write('# load: %s\n'%(args.load))
+sys.stdout.write('#\n')
+sys.stdout.write('#lo_ghz pa0 pa1 ')
+for pa in pas:
+    for mixer in ['01', '02', '11', '12']:
+        sys.stdout.write('ua%s_%03d '%(mixer, pa*100))
+    for po in ['0', '1']:
+        sys.stdout.write('vd%s_%03d '%(po, pa*100))
+    for po in ['0', '1']:
+        sys.stdout.write('id%s_%03d '%(po, pa*100))
+    for po in ['0', '1']:
+        sys.stdout.write('vg%s_%03d '%(po, pa*100))
+sys.stdout.write('\n')
+sys.stdout.flush()
+
+# init load controller and set desired load (default hot)
+load = namakanui.load.Load(datapath+'load.ini', time.sleep, namakanui.nop, simulate=0)
+if args.load == 'hot' or args.load == 'sky':
+    args.load = 'b%d_'%(band) + args.load
+load.move(args.load)
+
+# set agilent output to a safe level before setting ifswitch
+agilent = namakanui.agilent.Agilent(datapath+'agilent.ini', time.sleep, namakanui.nop, simulate=0)
+agilent.set_dbm(agilent.safe_dbm)
+agilent.set_output(1)
+ifswitch = namakanui.ifswitch.IFSwitch(datapath+'ifswitch.ini', time.sleep, namakanui.nop, simulate=0)
+ifswitch.set_band(band)
+
+# power up the cartridge
+cart = namakanui.cart.Cart(band, datapath+'band%d.ini'%(band), time.sleep, namakanui.nop, simulate=0)
+cart.power(1)
+cart.femc.set_cartridge_lo_pll_sb_lock_polarity_select(cart.ca, {'below':0, 'above':1}[args.lock_polarity])
+floog = agilent.floog * {'below':1.0, 'above':-1.0}[args.lock_polarity]
+
+
+# main loop
+for lo in los:
+    if not namakanui.util.tune(cart, agilent, None, lo):
+        continue
+    sys.stdout.write('%.3f '%(lo))
+    pa_drain_s = cart.state['pa_drain_s']
+    sys.stdout.write('%.3f %.3f '%(pa_drain_s[0], pa_drain_s[1]))
+    # average mixer currents
+    n = 10
+    uas = [0.0]*4
+    for i in range(n):
+        for po in range(2):
+            for sb in range(2):
+                uas[po*2 + sb] += cart.femc.get_sis_current(cart.ca,po,sb)*1e3
+    for i in range(4):
+        uas[i] /= n
+        sys.stdout.write('%.3f '%(uas[i]))
+        y[i][j].append(uas[i])
+    # amp feedback
+    pa_vd = [0.0]*2
+    pa_id = [0.0]*2
+    pa_vg = [0.0]*2
+    for po in range(2):
+        pa_vg[po] = cart.femc.get_cartridge_lo_pa_gate_voltage(cart.ca, po)
+        pa_vd[po] = cart.femc.get_cartridge_lo_pa_drain_voltage(cart.ca, po)
+        pa_id[po] = cart.femc.get_cartridge_lo_pa_drain_current(cart.ca, po)
+    sys.stdout.write('%.3f %.3f '%(pa_vd[0], pa_vd[1]))
+    sys.stdout.write('%.3f %.3f '%(pa_id[0], pa_id[1]))
+    sys.stdout.write('%.3f %.3f '%(pa_vg[0], pa_vg[1]))
+sys.stdout.write('\n')
+sys.stdout.flush()
+
+
+
+
