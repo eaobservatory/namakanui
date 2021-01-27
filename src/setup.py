41fca4162ead45f4e4434c7281748adebfa15b41
Ryan Berthold <r.berthold@eaobservatory.org>
2021-01-27 08:37:55 -1000

    use glob in setup.py for installed scripts

--- src/setup.py
+++ src/setup.py
@@ -1,21 +1,11 @@
 #!/local/python3/bin/python3
 from jac_sw import uae
+import glob
 
 uae.git_version_file('namakanui/version.py')
 
 uae.setup(
-    scripts = ['cartridge_task.py',
-               'dbm_table.py',
-               'fe_namakanui.py',
-               'mixer_iv.py',
-               'namakanui_gui.py',
-               'namakanui_task.py',
-               'pa_sweep.py',
-               'power_down.py',
-               'set_load.py',
-               'temp_mon.py',
-               'namakanui_tune.py',
-               'yfactor.py'],
+    scripts = glob.glob('namakanui_*.py'),
     packages = ['namakanui'],
 )
 
