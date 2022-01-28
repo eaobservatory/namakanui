6511a28f43eb37b2cdbe70b09b2bdad99b8bd459
Ryan Berthold <r.berthold@eaobservatory.org>
2022-01-28 11:50:06 -1000

    add debug logging to pcand and fix accept bug

--- src/namakanui_pcand.py
+++ src/namakanui_pcand.py
@@ -51,7 +51,12 @@ parser = argparse.ArgumentParser(
     formatter_class=argparse.RawTextHelpFormatter,
     description=namakanui.util.get_description(__doc__)
     )
+parser.add_argument('-v', '--verbose', action='store_true', help='enable debug logging output')
 args = parser.parse_args()
+
+if args.verbose:
+    logging.root.setLevel(logging.DEBUG)
+    log.setLevel(logging.DEBUG)
 
 cfg = namakanui.util.get_config('femc.ini')['femc']
 pcan_type = cfg['pcan_type'].lower()  # tcp or udp
@@ -62,25 +67,30 @@ pcand_port = int(cfg['pcand_port'])
 
 # connect to PCAN
 if pcan_type == 'tcp':
+    log.debug('creating tcp lan2can socket')
     lan2can = socket.socket()
 else:
+    log.debug('creating udp lan2can socket')
     lan2can = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 lan2can.settimeout(1)
+log.debug('lan2can.connect((%s, %d))', lan2can_ip, lan2can_port)
 lan2can.connect((lan2can_ip, lan2can_port))
 
 # bind a known port so PCAN can connect to us;
 # set SO_REUSEADDR so pcand can restart even if old socket stuck in TIME_WAIT
 if pcan_type == 'tcp':
+    log.debug('listening for can2lan on tcp port %d', can2lan_port)
     can2lan_listener = socket.socket()
     can2lan_listener.settimeout(5)
     can2lan_listener.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
     can2lan_listener.bind(('0.0.0.0', can2lan_port))
     can2lan_listener.listen()
-    can2lan = can2lan_listener.accept()
+    can2lan = can2lan_listener.accept()[0]
     can2lan.settimeout(1)
     can2lan_listener.shutdown(socket.SHUT_RDWR)
     can2lan_listener.close()
 else:
+    log.debug('binding can2lan on udp port %d', can2lan_port)
     can2lan = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
     can2lan.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
     can2lan.settimeout(1)
@@ -89,6 +99,7 @@ else:
 
 # create server listening socket on pcand_port;
 # set SO_REUSEADDR so pcand can restart even if old socket stuck in TIME_WAIT
+log.debug('listening for clients on tcp port %d', pcand_port)
 listener = socket.socket()
 listener.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
 listener.bind(('0.0.0.0', pcand_port))
@@ -96,12 +107,14 @@ listener.listen()
 clients = set()
 
 # main loop
+log.debug('entering main loop')
 while True:
     r,w,x = select.select([listener, can2lan] + list(clients), [], [], 0.0)
     r = set(r)
     if listener in r:
         r -= {listener}
-        client = listener.accept()
+        client = listener.accept()[0]
+        log.debug('accepted new client %s', client)
         client.setblocking(False)
         clients |= {client}
     if can2lan in r:
@@ -119,12 +132,14 @@ while True:
     for client in r:
         packet = client.recv(36)
         if len(packet) < 36:  # bad/lost/closed connection
+            log.debug('dropping client %s', client)
             client.close()
             clients -= {client}
         else:
             lan2can.send(packet)
 
 # only get here if lost PCAN connection, so clean up and exit with error
+log.debug('done, closing sockets.')
 listener.close()
 can2lan.close()
 for client in clients:
