74338c3eaff2408cc0a808e4ec73d3fa4cc7504c
Ryan Berthold <r.berthold@eaobservatory.org>
2022-03-24 11:24:45 -1000

    replace util IFTASK funcs with RFSMA/PMeter2 funcs

--- src/namakanui/util.py
+++ src/namakanui/util.py
@@ -189,119 +189,33 @@ class interval(object):
         return self.__str__()
     
 
-
-# The following functions are only defined if drama is already imported.
-if 'drama' in sys.modules:
-    import drama
-
-
-    def iftask_check_msg(action, msg):
-        '''Helper used by iftask_* functions to check obey replies.'''
-        if msg.reason != drama.REA_COMPLETE:
-            raise drama.BadStatus(drama.UNEXPMSG, f'{action} bad reply: {msg}')
-        if msg.status == 261456746:  # ACSISIF__ATTEN_ZERO
-            log.warning(f'{action} low attenuator setting')
-        elif msg.status != 0:
-            raise drama.BadStatus(msg.status, f'{action} bad status')
-
-
-    def iftask_setup(adjust, bw_mhz=1000, if_ghz=6, dcms=None):
-        '''
-        DRAMA function, must be called from an action.
-        Calls IFTASK.TEST_SETUP to configure ACSIS for the Namakanui receivers.
-        Arguments:
-            adjust: LEVEL_ADJUST 0=setup_only, 1=setup_and_level, 2=level_only
-            bw_mhz: BAND_WIDTH one of [250, 1000]
-            if_ghz: IF_FREQ one of [4, 5, 6, 7]
-            dcms: List of DCMs for BIT_MASK.  If None (default), use all 32 DCMs.
-        '''
-        adjust_valid = [0,1,2]
-        bw_mhz_valid = [250,1000]
-        if_ghz_valid = [4,5,6,7]
-        if adjust not in adjust_valid:
-            raise ValueError(f'adjust {adjust} not in {adjust_valid}')
-        if bw_mhz not in bw_mhz_valid:
-            raise ValueError(f'bw_mhz {bw_mhz} not in {bw_mhz_valid}')
-        if if_ghz not in if_ghz_valid:
-            raise ValueError(f'if_ghz {if_ghz} not in {if_ghz_valid}')
-        if dcms is None:
-            dcms = range(0,32)
-        bitmask = 0
-        for dcm in dcms:
-            bitmask |= 1<<dcm
-        setup_type = ['setup_only', 'setup_and_level', 'level_only'][adjust]
-        log.info('iftask_setup(%s, bw_mhz=%d, if_ghz=%d, bitmask=0x%x',
-                setup_type, bw_mhz, if_ghz, bitmask)
-        msg = drama.obey('IFTASK@if-micro', 'TEST_SETUP',
-                        NASM_SET='R_CABIN', BAND_WIDTH=bw_mhz, QUAD_MODE=4,
-                        IF_FREQ=if_ghz, LEVEL_ADJUST=adjust, BIT_MASK=bitmask).wait(90)
-        iftask_check_msg('IFTASK.TEST_SETUP', msg, log)
-        # iftask_setup
+def init_rfmsa_pmeters_49():
+    '''
+    Set both RFSMAs to send 4-9 GHz IFs to their first power meters.
+    Return PMeter2 instances (rfsma_p1, rfsma_p3), set to 6.5 GHz.
+    rfsma_p1: A14, EHT#1 (USB), chA=POL0, chB=POL1
+    rfsma_p3: A17, EHT#2 (LSB), chA=POL0, chB=POL1
+    '''
+    config = get_config('rfsma.ini')  # holds pmeter config also
+    rfsma_a14 = namakanui.rfsma.RFSMA(config, 'rfsma_a14', level=logging.DEBUG)
+    rfsma_a17 = namakanui.rfsma.RFSMA(config, 'rfsma_a17', level=logging.DEBUG)
+    rfsma_a14.set_pmeter_49()
+    rfsma_a17.set_pmeter_49()
+    rfsma_p1 = namakanui.pmeter2.PMeter2(config, 'rfsma_p1')
+    rfsma_p3 = namakanui.pmeter2.PMeter2(config, 'rfsma_p3')
+    rfsma_p1.set_ghz(6.5)
+    rfsma_p3.set_ghz(6.5)
+    return (rfsma_p1, rfsma_p3)
 
 
-    def iftask_set_bw(bw_mhz):
-        '''
-        DRAMA function, must be called from an action.
-        Calls IFTASK.SET_DCM_BW.
-        Arguments:
-            bw_mhz: MHZ one of [250, 1000]
-        '''
-        bw_mhz_valid = [250,1000]
-        if bw_mhz not in bw_mhz_valid:
-            raise ValueError(f'bw_mhz {bw_mhz} not in {bw_mhz_valid}')
-        log.info('iftask_set_bw(%d)', bw_mhz)
-        msg = drama.obey('IFTASK@if-micro', 'SET_DCM_BW', DCM=-1, MHZ=bw_mhz).wait(10)
-        iftask_check_msg('IFTASK.SET_DCM_BW', msg, log)
-        # iftask_set_bw
-
-
-    def iftask_set_lo2(lo2_mhz):
-        '''
-        DRAMA function, must be called from an action.
-        Calls IFTASK.SET_LO2_FREQ.
-        Can be quite slow, ~20s to set freqs + ~20s to set coax switches.
-        Arguments:
-            lo2_mhz: MHZ in range [6000, 10000]
-        '''
-        lo2_mhz_valid = interval(6000, 10000)
-        if lo2_mhz not in lo2_mhz_valid:
-            raise ValueError(f'lo2_mhz {lo2_mhz} not in {lo_mhz_valid}')
-        log.info('iftask_set_lo2(%g)', lo2_mhz)
-        msg = drama.obey('IFTASK@if-micro', 'SET_LO2_FREQ', LO2=-1, MHZ=lo2_mhz).wait(90)
-        iftask_check_msg('IFTASK.SET_LO2_FREQ', msg, log)
-        # iftask_set_lo2
-
-
-    def iftask_get_tp2(dcms, itime=0.1):
-        '''
-        DRAMA function, must be called from an action.
-        Calls IFTASK.WRITE_TP2 and returns list of power readings.
-        Arguments:
-            dcms: List of DCMs to return power readings for
-            itime: Integration time, seconds
-        '''
-        wtime = itime + 5
-        msg = drama.obey("IFTASK@if-micro", "WRITE_TP2", FILE="NONE", ITIME=itime).wait(wtime)
-        iftask_check_msg('IFTASK.WRITE_TP2', msg, log)
-        tps = []
-        for dcm in dcms:
-            tps.append(msg.arg['POWER%d'%(dcm)])
-        return tps
-        # iftask_get_tp2
-
-
-    def iftask_get_att(dcms):
-        '''
-        DRAMA function, must be called from an action.
-        Calls IFTASK.GET_DCM_ATTEN and returns list of attenuator counts.
-        Arguments:
-            dcms: List of DCMs to return attenuator counts for
-        '''
-        msg = drama.obey('IFTASK@if-micro', 'GET_DCM_ATTEN', DCM=-1).wait(5)
-        iftask_check_msg('IFTASK.GET_DCM_ATTEN', msg, log)
-        att = []
-        for dcm in dcms:
-            att.append(msg.arg['ATTEN%d'%(dcm)])
-        return att
-        # iftask_get_att
+def read_rfsma_pmeters(pmeters):
+    '''
+    Return readings for both channels of all pmeters as a flat list.
+    If pmeters = [rfsma_p1, rfsma_p3] (from init_rfmsa_pmeters_49),
+    then return = [usb_p0, usb_p1, lsb_p0, lsb_p1].
+    '''
+    # for multiple meters, separate read_init should be faster than read_power
+    for m in pmeters:
+        m.read_init()
+    return [p for m in pmeters for p in m.read_fetch()]
 
